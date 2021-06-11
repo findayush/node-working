526ad8787085b7fc8e6c85f00b697f91dd45b708
Ayush Kumar <Ayush_Kumar@epam.com>
2021-06-11 22:55:12 +0530

    process.nextTick added in flow

--- event-loop.js
+++ event-loop.js
@@ -11,7 +11,7 @@ fs.readFile("./test-file.txt", () => {
   setTimeout(() => console.log("Timer 2 finished"), 0);
   setTimeout(() => console.log("Timer 3 finished"), 3000);
   setImmediate(() => console.log("Immediate 2 finished"));
-  process.nextTick(() => console.log("process.nectTick()"));
+  process.nextTick(() => console.log("process.nextTick()"));
 
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
