c609120cde62806d6974c367b74ff08f2bd60b27
Ayush Kumar <Ayush_Kumar@epam.com>
2021-06-25 11:35:55 +0530

    Event loop order for threads added

--- event-loop.js
+++ event-loop.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const crypto = require("crypto");
 const start = Date.now();
-setTimeout(() => console.log("Timer 1 finished"), 0);
-setImmediate(() => console.log("Immediate 1 finished"));
+
 process.env.UV_THREADPOOL_SIZE = 1;
+
 fs.readFile("./test-file.txt", () => {
   console.log("I/O finished");
-  console.log("--------------Event loop below------------------------");
+  console.log("--------------Event loop callbacks------------------------");
 
   setTimeout(() => console.log("Timer 2 finished"), 0);
   setTimeout(() => console.log("Timer 3 finished"), 3000);
   setImmediate(() => console.log("Immediate 2 finished"));
   process.nextTick(() => console.log("process.nextTick()"));
+  console.log("next to pnt");
 
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
@@ -25,5 +26,17 @@ fs.readFile("./test-file.txt", () => {
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
   });
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password encrypted Sync");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password encrypted Sync");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password encrypted Sync");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password encrypted Sync");
 });
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
 console.log("Hello from top level code");
---
+++ events.js
@@ -0,0 +1,35 @@
+const EventEmitter = require("events");
+const http = require("http");
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmitter = new Sales();
+//const myEmitter = new EventEmitter();
+
+myEmitter.on("newSale", () => {
+  console.log("Sales done");
+});
+myEmitter.on("newSale", () => {
+  console.log("Client: Ayush");
+});
+myEmitter.on("newSale", (count) => {
+  console.log(`Items Sold ${count}`);
+});
+myEmitter.emit("newSale", 5);
+
+//////////////////////////////////server////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request here");
+  console.log(req.url);
+  res.end("API data here");
+});
+
+server.listen(8080, "127.0.0.1", () => {
+  console.log("Listening...");
+});
